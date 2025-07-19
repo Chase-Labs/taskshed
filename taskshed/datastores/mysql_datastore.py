8c7279f04095d02b100397bd1ddad68f34bd0f51
Jacob Strauss <jacob.d.strauss@gmail.com>
2025-07-19 18:29:29 +0100

    Refactor MySQLDataStore to improve timestamp handling and add unit tests for task scheduling

--- taskshed/datastores/mysql_datastore.py
+++ taskshed/datastores/mysql_datastore.py
@@ -1,15 +1,19 @@
 import asyncio
 import json
+import logging
 from collections.abc import Iterable
 from contextlib import asynccontextmanager
 from dataclasses import dataclass
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from typing import AsyncGenerator
 
 import aiomysql
 
 from taskshed.datastores.base_datastore import DataStore
 from taskshed.models.task_models import Task, TaskExecutionTime
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("MySQLDataStore")
 
 
 @dataclass(frozen=True, kw_only=True)
@@ -26,52 +30,53 @@ class MySQLDataStore(DataStore):
 
     _CREATE_TABLE_QUERY = """
     CREATE TABLE IF NOT EXISTS _taskshed_data (
-        `task_id` VARCHAR(63) NOT NULL,
-        `run_at` TIMESTAMP(6) NOT NULL,
-        `paused` TINYINT NOT NULL DEFAULT 0,
-        `callback_name` VARCHAR(63) NOT NULL,
-        `kwargs` JSON NOT NULL,
-        `schedule_type` ENUM('date', 'interval') NOT NULL,
-        `interval` FLOAT DEFAULT NULL,
-        `group_id` VARCHAR(63) DEFAULT NULL,
-    PRIMARY KEY (task_id),
-    UNIQUE INDEX task_id_UNIQUE (task_id ASC),
-    INDEX idx_group_id (group_id ASC),
-    INDEX idx_paused_run_at (paused ASC, run_at ASC)
+            `task_id` VARCHAR(63) NOT NULL,
+            `run_at` BIGINT UNSIGNED NOT NULL,
+            `paused` TINYINT NOT NULL DEFAULT 0,
+            `callback_name` VARCHAR(63) NOT NULL,
+            `kwargs` JSON NOT NULL,
+            `schedule_type` ENUM('date', 'interval') NOT NULL,
+            `interval` FLOAT DEFAULT NULL,
+            `group_id` VARCHAR(63) DEFAULT NULL,
+        PRIMARY KEY (`task_id`),
+        UNIQUE INDEX `task_id_UNIQUE` (`task_id` ASC),
+        INDEX `idx_group_id` (`group_id` ASC),
+        INDEX `idx_paused_run_at` (`paused` ASC, `run_at` ASC)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
     """
 
     _DELETE_TASKS_QUERY = """
     DELETE FROM _taskshed_data
     WHERE
-        task_id IN %s
+        `task_id` IN %s;
     """
 
     _DELETE_ALL_TASKS_QUERY = """
     DELETE FROM _taskshed_data
     WHERE
-        paused IN (0 , 1)
+        `paused` IN (0 , 1);
     """
 
     _DELETE_GROUP_TASKS_QUERY = """
     DELETE FROM _taskshed_data
     WHERE
-        group_id = %s
+        `group_id` = %s;
     """
 
     _INSERT_TASKS_WITHOUT_REPLACEMENT_QUERY = """
-    INSERT INTO _taskshed_data (task_id, run_at, paused, callback_name, kwargs, schedule_type, interval, group_id)
-    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+    INSERT INTO _taskshed_data (`task_id`, `run_at`, `paused`, `callback_name`, `kwargs`, `schedule_type`, `interval`, `group_id`)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
     """
 
     _INSERT_TASKS_WITH_REPLACEMENT_QUERY = """
-    INSERT INTO _taskshed_data (task_id, run_at, paused, callback_name, kwargs, schedule_type, interval, group_id)
+    INSERT INTO _taskshed_data (`task_id`, `run_at`, `paused`, `callback_name`, `kwargs`, `schedule_type`, `interval`, `group_id`)
     VALUES (%s, %s, %s, %s, %s, %s, %s, %s) AS new
         ON DUPLICATE KEY UPDATE
-            run_at = new.run_at,
-            callback_name = new.callback_name,
-            kwargs = new.kwargs,
-            paused = new.paused,
-            group_id = new.group_id
+            `run_at` = new.run_at,
+            `callback_name` = new.callback_name,
+            `kwargs` = new.kwargs,
+            `paused` = new.paused,
+            `group_id` = new.group_id;
     """
 
     _SELECT_TASKS_QUERY = """
@@ -80,7 +85,7 @@ class MySQLDataStore(DataStore):
     FROM
         _taskshed_data
     WHERE
-        task_id IN %s
+        `task_id` IN %s;
     """
 
     _SELECT_DUE_TASKS_QUERY = """
@@ -89,17 +94,17 @@ class MySQLDataStore(DataStore):
     FROM
         _taskshed_data
     WHERE
-        paused = 0 AND run_at <= %s 
+        `paused` = 0 AND `run_at` <= %s;
     """
 
     _SELECT_NEXT_WAKEUP_QUERY = """
     SELECT
-        MIN(run_at) AS next_wakeup
+        MIN(`run_at`) AS next_wakeup
     FROM
         _taskshed_data
     WHERE
-        paused = 0
-    LIMIT 1
+        `paused` = 0
+    LIMIT 1;
     """
 
     _SELECT_GROUP_TASKS_QUERY = """
@@ -108,31 +113,31 @@ class MySQLDataStore(DataStore):
     FROM
         _taskshed_data
     WHERE
-        group_id = %s
+        `group_id` = %s;
     """
 
     _UPDATE_TASKS_RUN_AT_QUERY = """
     UPDATE _taskshed_data
     SET
-        run_at = %s
+        `run_at` = %s
     WHERE
-        task_id = %s
+        `task_id` = %s;
     """
 
     _UPDATE_TASKS_PAUSED_STATUS_QUERY = """
     UPDATE _taskshed_data
     SET
-        paused = %s
+        `paused` = %s
     WHERE
-        task_id IN %s
+        `task_id` IN %s;
     """
 
     _UPDATE_GROUP_PAUSE_QUERY = """
     UPDATE _taskshed_data
     SET
-        paused = %s
+        `paused` = %s
     WHERE
-        group_id = %s
+        `group_id` = %s;
     """
 
     # -------------------------------------------------------------------------------- private methods
@@ -160,7 +165,7 @@ class MySQLDataStore(DataStore):
 
         return Task(
             task_id=row["task_id"],
-            run_at=row["run_at"],
+            run_at=self._convert_timestamp(row["run_at"]),
             paused=bool(row["paused"]),
             callback=row["callback_name"],
             kwargs=json.loads(row["kwargs"]),
@@ -168,6 +173,12 @@ class MySQLDataStore(DataStore):
             interval=interval,
             group_id=row["group_id"],
         )
+
+    def _convert_datetime(self, dt: datetime) -> int:
+        return int(dt.timestamp() * 1_000_000)
+
+    def _convert_timestamp(self, timestamp: int) -> datetime:
+        return datetime.fromtimestamp(timestamp / 1_000_000, tz=timezone.utc)
 
     # -------------------------------------------------------------------------------- public methods
 
@@ -203,39 +214,46 @@ class MySQLDataStore(DataStore):
         )
 
         async with self._get_cursor() as cursor:
-            await cursor.executemany(
-                query,
-                (
+            try:
+                await cursor.executemany(
+                    query,
                     (
-                        task.task_id,
-                        task.run_at,
-                        task.paused,
-                        task.callback,
-                        json.dumps(task.kwargs),
-                        task.schedule_type,
-                        task.interval_seconds(),
-                        task.group_id,
-                    )
-                    for task in tasks
-                ),
-            )
+                        (
+                            task.task_id,
+                            self._convert_datetime(task.run_at),
+                            task.paused,
+                            task.callback,
+                            json.dumps(task.kwargs),
+                            task.schedule_type,
+                            task.interval_seconds(),
+                            task.group_id,
+                        )
+                        for task in tasks
+                    ),
+                )
+            except Exception as e:
+                logger.error(f"Error adding tasks: {e}")
+                logger.error(f"Query: {cursor._last_executed}")
+                raise
 
     async def fetch_due_tasks(self, dt: datetime) -> list[Task]:
         async with self._lock:
             async with self._get_cursor() as cursor:
-                await cursor.execute(self._SELECT_DUE_TASKS_QUERY, (dt,))
+                await cursor.execute(
+                    self._SELECT_DUE_TASKS_QUERY, (self._convert_datetime(dt),)
+                )
                 rows = await cursor.fetchall()
 
             return [self._create_task(row) for row in rows]
 
-    async def fetch_next_wakeup(self) -> float | None:
+    async def fetch_next_wakeup(self) -> datetime | None:
         async with self._lock:
             async with self._get_cursor() as cursor:
                 await cursor.execute(self._SELECT_NEXT_WAKEUP_QUERY)
                 row = await cursor.fetchone()
 
             if row and row["next_wakeup"]:
-                return row["next_wakeup"]
+                return self._convert_timestamp(row["next_wakeup"])
 
     async def fetch_tasks(self, task_ids: Iterable[str]) -> list[Task]:
         async with self._get_cursor() as cursor:
@@ -253,7 +271,13 @@ class MySQLDataStore(DataStore):
         async with self._get_cursor() as cursor:
             await cursor.executemany(
                 self._UPDATE_TASKS_RUN_AT_QUERY,
-                ((task.run_at, task.task_id) for task in tasks),
+                (
+                    (
+                        self._convert_datetime(task.run_at),
+                        task.task_id,
+                    )
+                    for task in tasks
+                ),
             )
 
     async def update_tasks_paused_status(
